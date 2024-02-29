d5f3056dcebca91141f9983b308267955a135117
GiriprasathRangasamy <124856328+GiriprasathRangasamy@users.noreply.github.com>
2024-02-29 12:05:42 +0530

    Add files via upload

---
+++ 727722euit052/cc2/project/src/main/java/com/example/project/model/Finddealer.java
@@ -0,0 +1,27 @@
+package com.example.project.model;
+
+public class Finddealer {
+    String deelerName;
+    Double pricePerUnit;
+    long mobileNumber;
+    public String getDeelerName() {
+        return deelerName;
+    }
+    public void setDeelerName(String deelerName) {
+        this.deelerName = deelerName;
+    }
+    public Double getPricePerUnit() {
+        return pricePerUnit;
+    }
+    public void setPricePerUnit(Double pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+    public long getMobileNumber() {
+        return mobileNumber;
+    }
+    public void setMobileNumber(long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    
+    
+}
